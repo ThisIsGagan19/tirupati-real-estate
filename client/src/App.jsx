c2da3a16112071e22614de22983df3ca2b131815
ThisIsGagan19 <thisisgagan19@gmail.com>
2025-07-25 00:35:12 +0530

    initialize project with package.json and essential dependencies like express, mongoose

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,11 +6,13 @@ import SignUp from './pages/SignUp'
 import SignIn from './pages/Signin'
 import Profile from './pages/Profile'
 import About from './pages/About'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
