4e7b283c6ed98cd9dbc703d60838f494d795efc7
ThisIsGagan19 <thisisgagan19@gmail.com>
2025-07-24 23:20:56 +0530

    feat: initialize client application with React, Vite, and Tailwind CSS
    - Implemented basic pages: Home, About, SignIn, SignUp, Profile

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/Signin'
+import Profile from './pages/Profile'
+import About from './pages/About'
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
