e1c7ff2d2fd773dd5c861d3f7b04d5937b856dea
ThisIsGagan19 <thisisgagan19@gmail.com>
2025-08-12 22:52:51 +0530

    implement user authentication with signup and signin functionality; add redux for state management and persist user session

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -1,17 +1,42 @@
 import User from "../models/user.models.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
+
 
 export const signup = async (req, res, next) => {
   const { username, password, email } = req.body;
   const hashedPassword = await bcryptjs.hashSync(password, 10);
 
-  const newUser = new User({ username, password : hashedPassword, email });
+  const newUser = new User({ username, password: hashedPassword, email });
   try {
     await newUser.save();
     res.status(201).json("User created successfully");
   } catch (error) {
-    next(errorHandler(500,error.message || "Internal Server Error"));
+    next(errorHandler(500, error.message || "Internal Server Error"));
   }
 };
 
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found"));
+
+    const validPassword = await bcryptjs.compareSync(
+      password,
+      validUser.password
+    );
+    if (!validPassword) return next(errorHandler(401, "Invalid credentials"));
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const {password : pass, ...rest} = validUser._doc;
+
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(errorHandler(500, error.message || "Internal Server Error")); // differ from code
+  }
+};
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -71,14 +71,14 @@ function SignUp() {
           onChange={handleChange}
         />
         <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-          Sign Up
+          {loading ? "Loading..." : "Sign Up"}
         </button>
       </form>
 
       <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
         <Link to={"/sign-in"}>
-        <span className="text-blue-700">Sign In</span>
+          <span className="text-blue-700">Sign In</span>
         </Link>
       </div>
       {/* {error && <p className="text-red-500 m">{error}</p>} */}
